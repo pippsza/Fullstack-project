7a1a1b1122d0a0954a51feca078cb10a69e0faeb
pippsza <bastahov18@gmail.com>
2025-06-26 19:49:25 +0300

    added container component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,16 @@
 import AppBar from "../AppBar/AppBar.jsx";
 import css from "./Header.module.css";
+import Container from "../container/container.jsx";
 export default function Header() {
   return (
-    <div className={css.background}>
-      <div>
-        <h1>svg</h1>
-        <h1>Tasteorama </h1>
+    <Container>
+      <div className={css.background}>
+        <div>
+          <h1>svg</h1>
+          <h1>Tasteorama </h1>
+        </div>
+        <AppBar></AppBar>
       </div>
-      <AppBar></AppBar>
-    </div>
+    </Container>
   );
 }
---
+++ src/components/container/container.jsx
@@ -0,0 +1,4 @@
+import css from "./container.module.css";
+export default function Container({ children }) {
+  return <div className={css.container}>{children}</div>;
+}
