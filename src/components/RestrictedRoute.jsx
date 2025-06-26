7242b303d9ba0b440d3d5d5bd4160b4bcf7a78cf
pippsza <bastahov18@gmail.com>
2025-06-26 21:22:15 +0300

    new changes to routes

--- src/components/RestrictedRoute.jsx
+++ src/components/RestrictedRoute.jsx
@@ -1,6 +1,6 @@
 import { Navigate } from "react-router-dom";
 
-export default function PrivateRoute({ component, redirectTo }) {
+export default function RestrictedRoute({ component, redirectTo }) {
   const isLoggedIn = null;
 
   return isLoggedIn ? <Navigate to={redirectTo} /> : component;
--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,3 +1,11 @@
 export default function ProfilePage() {
-  return <h1>profile page</h1>;
+  return (
+    <>
+      <h1>profile page</h1>
+      <h1>Profile navigation</h1>
+      <h1>RecipesList</h1>
+      <h1>RecipeCard</h1>
+      <h1>loadmore button</h1>
+    </>
+  );
 }
