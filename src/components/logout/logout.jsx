97695214f6e27b5fa57d24ba2851b6d2b9f9eec2
pippsza <bastahov18@gmail.com>
2025-06-28 15:49:52 +0300

    123

--- src/components/logout/logout.jsx
+++ src/components/logout/logout.jsx
@@ -4,10 +4,12 @@ export default function Logout() {
   const userName = "Max";
   const cutUserName = () => {};
   return (
-    <div>
-      <div></div>
-      <p></p>
-      <div></div>
+    <div className={css.container}>
+      <div>
+        <div className={css.icon}></div>
+        <p className={css.name}>{userName}</p>
+      </div>
+      <div className={css.stick}></div>
       <svg className={css.svg}>
         <use href={`${sprite}#icon-exit`}></use>
       </svg>
