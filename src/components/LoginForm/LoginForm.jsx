1a6ae7a2a00f9e0b0f7de9d712d14571e4a9ebc1
zloymaskalik <usvplus@gmail.com>
2025-07-02 18:29:49 +0300

    scc-update

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -5,32 +5,32 @@ import { Link } from "react-router-dom";
 import { nanoid } from "@reduxjs/toolkit";
 import * as Yup from "yup";
 import { toast } from "react-toastify";
-import { useNavigate } from "react-router-dom";
-import Svg from '../Svg/svg.jsx'
+import Svg from "../Svg/svg.jsx";
 
-import { login } from '../../redux/auth/operations';
+import { login } from "../../redux/auth/operations";
 
 import css from "./LoginForm.module.css";
 import { selectIsLoggedIn } from "../../redux/auth/selectors";
 
 const LoginSchema = Yup.object().shape({
   email: Yup.string()
-    .email("Please enter a valid email")
-    .required("Email is required field!"),
-  password: Yup.string().required("Password is required field!"),
+    .email("Invalid email format")
+    .min(3, "Must be min 3 chars")
+    .required("This field is required")
+    .max(50, "Must be less then 50 chars"),
+  password: Yup.string()
+    .min(8, "Must be min 8 chars")
+    .required("This field is required")
+    .max(50, "Must be less then 50 chars"),
 });
 
 const initialValues = { email: "", password: "" };
 
 export default function LoginForm() {
-
   const [showPassword, setShowPassword] = useState(false);
 
-
-
-  const isLogged = useSelector(selectIsLoggedIn)
-  console.log(isLogged, "islogged?")
-  const navigate = useNavigate();
+  const isLogged = useSelector(selectIsLoggedIn);
+  console.log(isLogged, "islogged?");
   const emailFieldId = nanoid();
   const passwordFieldId = nanoid();
 
@@ -41,7 +41,6 @@ export default function LoginForm() {
       .unwrap()
       .then(() => {
         toast.success("Login successful!");
-        navigate('/private');
       })
       .catch(() => {
         toast.error(
@@ -57,54 +56,56 @@ export default function LoginForm() {
       onSubmit={handleSubmit}
       validationSchema={LoginSchema}
     >
-      <Form className={css.loginForm}>
-        <h2 className={css.loginText}>Login</h2>
-        <label className={css.loginLabel} htmlFor={emailFieldId}>
-          Enter your email address
-        </label>
-        <Field
-          className={css.loginInput}
-          type="email"
-          name="email"
-          placeholder="email@gmail.com"
-          id={emailFieldId}
-        />
-        <ErrorMessage className={css.error} name="email" component="span" />
-        <label
-          className={`${css.loginLabel} ${css.loginLabelWithSpace}`}
-          htmlFor={passwordFieldId}
-        >
-          Enter your password
-        </label>
-        <div>
-          <Field
-            className={css.loginInput}
-            type={showPassword ? "text" : "password"}
-            name="password"
-            placeholder="&middot;&middot;&middot;&middot;&middot;&middot;&middot;&middot;&middot;&middot;&middot;"
-            id={passwordFieldId}
-          />
-          <Svg
-            name={showPassword ? "eye" : "close-eye"}
-            styles={css.iconEye}
-            onClick={() => setShowPassword((prev) => !prev)}
-          />
-        </div>
-        <ErrorMessage
-          className={css.errorPass}
-          name="password"
-          component="span"
-        />
-        <button className={css.loginBtn} type="submit">
-          Log In
-        </button>
-        <p className={css.regText}>
-          Don`t have an account? &nbsp;
-          <Link className={css.regLink} to="/auth/register">
-            Register
-          </Link>
-        </p>
-      </Form>
+        <Form className={css.loginForm}>
+          <h2 className={css.loginText}>Login</h2>
+          <label className={css.loginLabel} htmlFor={emailFieldId}>
+            Enter your email address
+          </label>
+          <div className={css.passwordField}>
+            <Field
+              className={css.loginInput}
+              type="email"
+              name="email"
+              placeholder="email@gmail.com"
+              id={emailFieldId}
+            />
+            <ErrorMessage className={css.error} name="email" component="span" />
+          </div>
+          <label
+            className={`${css.loginLabel} ${css.loginLabelWithSpace}`}
+            htmlFor={passwordFieldId}
+          >
+            Enter your password
+          </label>
+          <div className={css.passwordField}>
+            <Field
+              className={css.loginInput}
+              type={showPassword ? "text" : "password"}
+              name="password"
+              placeholder="&middot;&middot;&middot;&middot;&middot;&middot;&middot;&middot;&middot;&middot;&middot;"
+              id={passwordFieldId}
+            />
+            <Svg
+              name={showPassword ? "eye" : "close-eye"}
+              styles={css.iconEye}
+              onClick={() => setShowPassword((prev) => !prev)}
+            />
+            <ErrorMessage
+              className={css.errorPass}
+              name="password"
+              component="span"
+            />
+          </div>
+          <button className={css.loginBtn} type="submit">
+            Log In
+          </button>
+          <p className={css.regText}>
+            Don`t have an account? &nbsp;
+            <Link className={css.regLink} to="/auth/register">
+              Register
+            </Link>
+          </p>
+        </Form>
     </Formik>
   );
 }
